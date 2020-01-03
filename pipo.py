fe9649a1004ff9b91340244267f3f2258996b6dd
Emmanuelle Quilliet <emmanuelle_quilliet@ultimatesoftware.com>
2020-01-03 09:47:16 +0100

    des trucs

--- pipo.py
+++ pipo.py
@@ -3,3 +3,16 @@ print("Hello World")
 print('yo mama')
 
 print('des machin')
+
+import os
+import sys
+from glob import glob
+from pathlib import Path
+
+cur_dir = Path(os.getcwd()).expanduser()
+print(cur_dir)
+
+views_file = list(cur_dir.glob('**/%s.sql' % '*'))
+
+views_name = tuple(f.name.strip(".sql") for f in views_file)
+print(views_name)
